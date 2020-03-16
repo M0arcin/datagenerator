9d879eff08e953575304e03ef7df611a6abf52dc
Marcin Głowacki <marcinglowacki01@gmail.com>
2020-03-16 16:31:04 +0100

    Add hard coded response at "/generate/json"

--- src/main/java/pl/datagenerator/datagenerator/Country.java
+++ src/main/java/pl/datagenerator/datagenerator/Country.java
@@ -2,6 +2,8 @@ package pl.datagenerator.datagenerator;
 
 
 public class Country {
+
+
     private final String _type;
     private final int _id;
     private final String key;
@@ -10,21 +12,22 @@ public class Country {
     private final String iata_airport_code;
     private final String type;
     private final String country;
-    private final String geo_position;
+    private GeoPosition geo_position;
     private final int location_id;
     private final boolean inEurope;
     private final String countryCode;
     private final boolean coreCountry;
     private final String distance;
 
-    public Country(String type, int id, String key, String name, String fullName, String iata_airport_code, String type1, String country, String geo_position, int location_id, boolean inEurope, String countryCode, boolean coreCountry, String distance) {
-        _type = type;
-        _id = id;
+
+    public Country(String _type, int _id, String key, String name, String fullName, String iata_airport_code, String type, String country, GeoPosition geo_position, int location_id, boolean inEurope, String countryCode, boolean coreCountry, String distance) {
+        this._type = _type;
+        this._id = _id;
         this.key = key;
         this.name = name;
         this.fullName = fullName;
         this.iata_airport_code = iata_airport_code;
-        this.type = type1;
+        this.type = type;
         this.country = country;
         this.geo_position = geo_position;
         this.location_id = location_id;
@@ -33,7 +36,6 @@ public class Country {
         this.coreCountry = coreCountry;
         this.distance = distance;
     }
-
 
     public String get_type() {
         return _type;
@@ -67,8 +69,12 @@ public class Country {
         return country;
     }
 
-    public String getGeo_position() {
+    public GeoPosition getGeo_position() {
         return geo_position;
+    }
+
+    public void setGeo_position(GeoPosition geo_position) {
+        this.geo_position = geo_position;
     }
 
     public int getLocation_id() {
