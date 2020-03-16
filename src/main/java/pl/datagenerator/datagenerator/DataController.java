49cff158f98692f5984610b1cf33c023515c71c3
Marcin Głowacki <marcinglowacki01@gmail.com>
2020-03-16 18:45:11 +0100

    Generate multiple data sets of countries

--- src/main/java/pl/datagenerator/datagenerator/DataController.java
+++ src/main/java/pl/datagenerator/datagenerator/DataController.java
@@ -3,7 +3,11 @@ package pl.datagenerator.datagenerator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -11,10 +15,14 @@ public class DataController {
 
     private final String type = "Position";
 
-    @GetMapping("/generate/json")
-    public Country getCountries(){
-            GeoPosition geoPosition2 = new GeoPosition( 51.0855422,  16.9987442);
-            return new Country(type, 5, null, "Oksywska", "Oksywska, Poland", null, "location", "Poland", geoPosition2 , 757423, true, "PL", true, null);
+    @GetMapping("/generate/json/{size}")
+    public List<Country> getCountries(@PathVariable int size){
+        List <Country> countryList = new ArrayList<>();
+
+        for(int i = size; i>0; i--){
+            countryList.add(new Country(type, 65483214, null, "Oksywska", "Oksywska, Poland", null, "location", "Poland", new GeoPosition( 51.0855422,  16.9987442) , 757423, true, "PL", true, null));
+        }
+            return countryList;
         }
 
 
