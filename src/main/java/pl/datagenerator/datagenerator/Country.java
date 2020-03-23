2bb3af709315f1a0122bf75d55fe9bc329c7e7af
Marcin Głowacki <marcinglowacki01@gmail.com>
2020-03-23 16:05:52 +0100

    Add JPA naming conventions to Country class

--- src/main/java/pl/datagenerator/datagenerator/Country.java
+++ src/main/java/pl/datagenerator/datagenerator/Country.java
@@ -1,24 +1,59 @@
 package pl.datagenerator.datagenerator;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Country {
 
-
-    private final String _type;
+    @Id
     private int _id;
-    private final String key;
-    private final String name;
-    private final String fullName;
-    private final String iata_airport_code;
-    private final String type;
-    private final String country;
-    private GeoPosition geo_position;
-    private final int location_id;
-    private final boolean inEurope;
-    private final String countryCode;
-    private final boolean coreCountry;
-    private final String distance;
 
+    @Column
+    private String _type;
+
+    @Column
+    private String key;
+
+    @Column
+    private String name;
+
+    @Column
+    private String fullName;
+
+    @Column
+    private String iata_airport_code;
+
+    @Column
+    private String type;
+
+    @Column
+    private String country;
+
+    @Embedded
+    private GeoPosition geo_position;
+
+    @Column
+    private int location_id;
+
+    @Column
+    private boolean inEurope;
+
+    @Column
+    private String countryCode;
+
+    @Column
+    private boolean coreCountry;
+
+    @Column
+    private String distance;
+
+
+    public Country(){
+
+    }
 
     public Country(String _type, int _id, String key, String name, String fullName, String iata_airport_code, String type, String country, GeoPosition geo_position, int location_id, boolean inEurope, String countryCode, boolean coreCountry, String distance) {
         this._type = _type;
@@ -36,6 +71,8 @@ public class Country {
         this.coreCountry = coreCountry;
         this.distance = distance;
     }
+
+
 
     public String get_type() {
         return _type;
@@ -99,5 +136,53 @@ public class Country {
 
     public void set_id(int _id) {
         this._id = _id;
+    }
+
+    public void set_type(String _type) {
+        this._type = _type;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setIata_airport_code(String iata_airport_code) {
+        this.iata_airport_code = iata_airport_code;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLocation_id(int location_id) {
+        this.location_id = location_id;
+    }
+
+    public void setInEurope(boolean inEurope) {
+        this.inEurope = inEurope;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setCoreCountry(boolean coreCountry) {
+        this.coreCountry = coreCountry;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
     }
 }
